419c4219cb7aee50c2f39a2f10ad7f6ca4e7fd94
Ieuan Stanley <istanley@imperosoftware.com>
2018-09-25 12:48:20 +0100

    Updated to include solution for Day 1

--- src/app/Day1.ts
+++ src/app/Day1.ts
@@ -1,14 +1,47 @@
+declare global {
+    interface Array<T> {
+        reduceSearch(
+            reducer: (accumulator: number, current: number) => number,
+            condition: (currentValue: number) => boolean,
+        ): number;
+    }
+}
+
+if (!Array.prototype.reduceSearch) {
+    Array.prototype.reduceSearch =
+        function<T> (reducer, condition, index = 0, currentValue = 0): number {
+            currentValue = reducer(currentValue, this[index]);
+
+            if (condition(currentValue)) {
+                return index + 1;
+            }
+
+            return this.reduceSearch(
+                reducer,
+                condition,
+                ++index,
+                currentValue
+            );
+        };
+}
+
+const valueOfInstruction = (parenthesis: string): number =>
+    parenthesis === '(' ? 1 : -1;
+
+const sumReduce = (accumulator: number, current: number) =>
+    accumulator + current;
+
 export function calculateFloor(instructions: string): number {
+    return instructions.split('')
+        .map(valueOfInstruction)
+        .reduce(sumReduce);
+}
 
-    const instructionsAsArray = () => {
-        return instructions.split('');
-    };
+export function findBasementInstruction(instructions: string): number {
+    const isBasement = (currentFloor) =>
+        currentFloor === -1;
 
-    const valueOf = (parenthesis) => {
-        return parenthesis === '(' ? 1 : -1;
-    };
-
-    // Use Generators?
-
-    return 0;
+    return instructions.split('')
+        .map(valueOfInstruction)
+        .reduceSearch(sumReduce, isBasement);
 }
